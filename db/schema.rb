4f75840d5e5bc9f05c78ed2921c726530c13ae63
alvaro <4lvaro.up@gmail.com>
2020-02-16 09:26:54 -0300

    ADD: navigation and current user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_120335) do
+ActiveRecord::Schema.define(version: 2020_02_16_122405) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_02_16_120335) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stars"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
