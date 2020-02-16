66802780a395e3ea0c17bde38c2442d48d461433
alvaro <4lvaro.up@gmail.com>
2020-02-16 08:53:59 -0300

    ADD: actions update,edit and delete

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -12,10 +12,6 @@ class PlaysController < ApplicationController
     def show
 
     end
-
-    def edit
-
-    end
     
     def create
         @play = Play.new(play_params)
@@ -24,6 +20,24 @@ class PlaysController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def edit
+        
+    end
+
+    def update
+        if @play.update(find_play)
+            redirect_to play_path(@play)
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @play.destroy
+        redirect_to root_path
+        
     end
 
     private
